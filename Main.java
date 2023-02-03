522340a4e4f202222bfadeb9edd3940f591ce5b8
kayuk <kayuk@LAPTOP-GGLPNDUC>
2023-02-03 07:54:19 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.sparta;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] iArr = {1, 2};
+//        float f = 21.0f;
+//        double d = 2.0;
+//        long l = 12345;
+//        byte b = 1;
+//        short s = 1;
+//        boolean boolean1 = false;
+
+
+
+        System.out.println("Hello world!");
+
+    }
+}
